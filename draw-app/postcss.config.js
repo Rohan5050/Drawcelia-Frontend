d54f532f3bf6e90bfb899945c281f9429ec09eb4
Rohan Vohra <rohanvohra055@gmail.com>
2025-03-18 16:27:59 +0530

    Fix : Updated to mjs file

--- draw-app/postcss.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    '@tailwindcss/postcss7-compat': {},
-      autoprefixer: {},
-  },
-};
-
-export default config;
